f5f3f98dd16f95ac9e6b00921b2ee9ce5eaa3003
zohaib2k2 <zohaib.zafar2k2@gmail.com>
2025-12-30 10:05:35 +0500

    Add the env variables.

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 
+import express from 'express';
+import {PORT} from './config/env.js'
+
+
+const app = express();
+
+
+app.get('/', (req, res) => {
+    res.send('<h1 style="color:green;padding:5rem;margin:4rem">Welcome to the subscription tracker API</h1>');
+});
+
+app.listen(PORT, () => {
+    console.log(`Subscription-traction API server running on port ${PORT}`)}
+);
+
+export default app;
